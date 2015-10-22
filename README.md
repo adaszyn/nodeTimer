79a513374e4dbff31d9f19bf1c46b19492b915c4
wojciech <wojciech.adaszynski@gmail.com>
2015-10-22 18:58:22 +0200

    initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# nodeTimer
+Simple timer written in nodeJS. To setup new timer for 2 minutes and 10 seconds:
+```bash
+$ node timer.js -m 2 -s 10
+```
---
+++ timer.js
@@ -0,0 +1,36 @@
+#!/usr/local/bin/node
+var commander = require('commander');
+var ProgressBar = require('progress');
+var player = require('play-sound')(opts = {})
+
+var timeSum = 0;
+
+commander
+  .version('0.0.1')
+  .usage('[options] <file ...>')
+  .option('-s, --seconds <n>', 'duration (integer value)', parseInt)
+  .option('-m, --minutes <n>', 'duration (integer value)', parseInt)
+  .parse(process.argv);
+
+
+if (commander.minutes) {
+    timeSum = commander.minutes * 60;
+}
+if (commander.seconds) {
+  timeSum = timeSum + commander.seconds;
+}
+if (timeSum <= 0) {
+  console.log('Wrong parameters!');
+  process.exit();
+}
+console.log('Timer set to ' + timeSum + ' seconds.');
+var bar = new ProgressBar(':bar', { total: timeSum, width: 40 });
+var timer = setInterval(function () {
+  bar.tick();
+  if (bar.complete) {
+    player.play('./timer.mp3', function(err){
+      if (err) console.log(err);
+    });
+    clearInterval(timer);
+  }
+}, 1000);
